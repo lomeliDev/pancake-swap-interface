21707bfd8ab23a2b9674806471af5e6394f422e6
Miguel Lomeli <lomelidev@MacBook-Pro-de-Miguel.local>
2021-01-13 16:54:32 -0600

    Cambio de direcion del router

--- src/state/swap/hooks.ts
+++ src/state/swap/hooks.ts
@@ -91,7 +91,7 @@ export function tryParseAmount(value?: string, currency?: Currency): CurrencyAmo
 const BAD_RECIPIENT_ADDRESSES: string[] = [
   '0xBCfCcbde45cE874adCB698cC183deBcF17952812', // v2 factory
   '0xf164fC0Ec4E93095b804a4795bBe1e041497b92a', // v2 router 01
-  '0x05fF2B0DB69458A0750badebc4f9e13aDd608C7F' // v2 router 02
+  '0xcbdF26BfC8a3a34379f5d747De11Eb5B53f9A6Ca' // v2 router 02
 ]
 
 /**
